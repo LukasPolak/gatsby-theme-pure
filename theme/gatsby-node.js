7c21970747b6334ff2f32170495f0f990f5756b5
Lukas Polak <lukash.polak@gmail.com>
2019-07-24 18:15:33 +0200

    add `articles` to `gatsby-node.js`

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -6,16 +6,35 @@ exports.createPages = ({ actions, reporter }) => {
     path: "/",
     component: require.resolve("./src/templates/page.js"),
     context: {
-      heading: "Your Theme Here",
-      content: `
-        <p>
-          Use this handy theme example as the basis for your own amazing theme!
-        </p>
-        <p>
-          For more information, see 
-          <a href="https://themejam.gatsbyjs.org">themejam.gatsbyjs.org</a>.
-        </p>
-      `,
+      articles: [
+        {
+          heading:
+            "What lower Netflix pricing tells us about competing in India",
+          content: `
+          <p>
+          At a conference in New Delhi early last year, Netflix CEO Reed Hastings was confronted with a que...
+          </p>
+        `,
+        },
+        {
+          heading:
+            "Benchling’s software for managing biotech research nabs $34.5 million",
+          content: `
+          <p>
+          In a field where the laboratory notebook is still considered the state of the art, it’s no wonder a company like Benchling, which provides software for managing life sciences research was abl...
+          </p>
+        `,
+        },
+        {
+          heading:
+            "Duo’s Wendy Nather to talk security at TC Sessions: Enterprise",
+          content: `
+          <p>
+          When it comes to enterprise security, how do you move fast without breaking things? Enter Duo’s Wendy Nather, who will join us at TC Sessions: Enterprise in San Francisco on September 5, wher...
+          </p>
+        `,
+        },
+      ],
     },
   })
 }
